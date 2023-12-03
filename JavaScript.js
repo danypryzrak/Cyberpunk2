645d8d7e3cdecc2958b39a8b1abf015f4e501456
danypryzrak <116654935+danypryzrak@users.noreply.github.com>
2023-12-03 13:13:08 +0100

    h

--- JavaScript.js
+++ JavaScript.js
@@ -1,7 +1,14 @@
-"use strict";
-const age = 3
-if (age === 3 && age > 3) {
-    console.log(true)
-} else {
+function findMatches(...args) {
+    const matches = []; // Don't change this line
+    firsArray = args[0]
+    console.log(firsArray)
     
+  for (let i = 1; i < args; i++) {
+  
+  }
+
+  // Change code above this line
+  return matches;
 }
+
+findMatches([1, 2, 3, 4, 5], 1, 8, 2, 7)
